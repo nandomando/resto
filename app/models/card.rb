720230f8abd9e140095b09c230eb5e9ad92e800e
fernando orozco <fernando.orozco@hotmail.fr>
2019-12-07 20:59:25 +0100

    done

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: :destroy
   belongs_to :restaurant
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  has_many :users, through: cards
 end
