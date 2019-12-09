099b5baf5aeb2a43a6fb026782c1e4b6e8ab2d1c
fernando orozco <fernando.orozco@hotmail.fr>
2019-12-09 21:07:47 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated do
+    root to: "restaurants#index", as: :authenticated
+  end
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :restaurants
